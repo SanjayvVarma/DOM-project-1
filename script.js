76048c0898162e51c194f264e1fc001776f03d7b
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-01-06 00:28:23 +0530

    1st dom project

---
+++ script.js
@@ -0,0 +1,11 @@
+let div = document.getElementById("root");
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.height = "200px";
+div.style.width = "300px";
+ 
+let h1 = document.querySelector("h1");
+h1.style.fontSize = "18px";
+h1.style.fontWeight = "bold";
+h1.style.padding = "10px";
+h1.style.color = "#0000FF"
